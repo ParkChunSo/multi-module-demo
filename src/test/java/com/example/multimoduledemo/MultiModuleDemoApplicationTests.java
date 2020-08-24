04f0d82affea120717d647612d263c2d678bf791
DESKTOP-2389N04\chun <cnsth123@gmail.com>
2020-08-24 12:55:01 +0900

    git init

---
+++ src/test/java/com/example/multimoduledemo/MultiModuleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multimoduledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModuleDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
